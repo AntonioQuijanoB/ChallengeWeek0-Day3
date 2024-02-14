3ebc599817bf85d3f2abd831d52f803d93d5a844
Antonio Quijano <antonio.quijanoberne@gmail.com>
2024-02-14 18:37:10 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import { strictEquals } from './prueba.js';
+console.log(strictEquals(1, 1)); //true
+console.log(strictEquals(NaN, NaN)); // false --> Rule
+console.log(strictEquals(0, -0)); // true --> Rule
+console.log(strictEquals(-0, 0)); // true --> Rule
+console.log(strictEquals(1, '1')); // false
+console.log(strictEquals(true, false)); // false
+console.log(strictEquals(false, false)); // true
+console.log(strictEquals('water', 'oil'));
